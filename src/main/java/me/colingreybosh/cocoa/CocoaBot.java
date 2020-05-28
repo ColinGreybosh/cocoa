a97651ce0cb0352e05f579908f5b333f41fd5cba
Colin Greybosh <greybosh@mit.edu>
2020-05-28 15:30:19 -0400

    Removed private listener class and added main

--- src/main/java/me/colingreybosh/cocoa/CocoaBot.java
+++ src/main/java/me/colingreybosh/cocoa/CocoaBot.java
@@ -1,12 +1,36 @@
 package me.colingreybosh.cocoa;
 
+import java.util.Set;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
 /**
+ * An ADT representing a discord bot that interacts with users of the 
+ * Slugfest Discord server.
  * 
  * @author Colin Greybosh
  *
  */
-public class CocoaBot {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class CocoaBot extends Bot {
+    
+    public CocoaBot(String token) {
+        super(token, Set.of(new CocoaBotListener()), Set.of(GatewayIntent.GUILD_MEMBERS));
+        // TODO listeners and intents
+    }
+    
+    public static void main(String[] arguments) {
+        System.err.println("Creating bot...");
+        final CocoaBot bot = new CocoaBot(Constants.TOKEN);
+        System.err.println("Bot created!");
+        System.err.println("Connecting bot...");
+        try {
+            bot.start();
+            System.err.println("Bot logged in!");
+        } catch (LoginException | IllegalArgumentException | InterruptedException e) {
+            System.err.println("Bot failed to log in!");
+            e.printStackTrace();
+        }
     }
 }
