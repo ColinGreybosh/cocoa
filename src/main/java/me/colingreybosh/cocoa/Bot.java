99dfc99311ef27dc3072c6c2dfb15671c79ac03b
Colin Greybosh <greybosh@mit.edu>
2020-05-28 11:47:49 -0400

    Added specifications

---
+++ src/main/java/me/colingreybosh/cocoa/Bot.java
@@ -0,0 +1,139 @@
+package me.colingreybosh.cocoa;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+/**
+ * An ADT representing a basic Discord bot.
+ * 
+ * @author Colin Greybosh
+ *
+ */
+public class Bot {
+
+    private final String token;
+    private final Set<ListenerAdapter> listeners;
+    private final Set<GatewayIntent> intents;
+    
+    /*
+     * Abstraction Function:
+     *   AF(token, listeners, intents) = a bot running on the Discord appication denoted by the `token`
+     *                                   with all the listener methods in `listeners` that operates with
+     *                                   all intent flags in `intents` enabled along with JDA's default
+     *                                   intents.
+     *   
+     * Representation Invariant:
+     *   true
+     * 
+     * Safety From Representation Exposure:
+     *   all fields are private and final
+     *   all sets are returned within an unmodifiable wrapper
+     *   the objects contained in the representation sets are immutable
+     *   the collections received in the constructors are defensively copied
+     */
+    
+    /**
+     * Create a new bot.
+     * 
+     * @param token A Discord bot application token
+     */
+    public Bot(String token) {
+        this.token = token;
+        this.listeners = new HashSet<>();
+        this.intents = new HashSet<>(Set.of(GatewayIntent.GUILD_MEMBERS));
+        checkRep();
+    }
+    
+    public Bot(String token, Set<ListenerAdapter> listeners) {
+        this.token = token;
+        this.listeners = new HashSet<>(listeners);
+        this.intents = new HashSet<>(Set.of(GatewayIntent.GUILD_MEMBERS));
+        checkRep();
+    }
+    
+    public Bot(String token, Set<ListenerAdapter> listeners, Set<GatewayIntent> intents) {
+        this.token = token;
+        this.listeners = new HashSet<>(listeners);
+        this.intents = new HashSet<>(intents);
+        checkRep();
+    }
+    
+    /**
+     * Asserts the representation invariant.
+     */
+    private void checkRep() {
+       assert token != null;
+       assert listeners != null;
+       assert intents != null;
+    }
+    
+    /**
+     * Connect to Discord.
+     * 
+     * @throws LoginException If this bot's token is invalid
+     * @throws IllegalArgumentException If this bot's token is empty or null
+     * @throws InterruptedException If this bot's thread was interrupted while waiting to connect to Discord
+     */
+    public JDA start() throws LoginException, IllegalArgumentException, InterruptedException {
+        return JDABuilder.createDefault(token, intents).addEventListeners(listeners).build().awaitReady();
+    }
+    
+    /**
+     * Get this bot's token.
+     * 
+     * @return This bot's token
+     */
+    public String getToken() {
+        return token;
+    }
+    
+    /**
+     * Add a listener to this bot.
+     * 
+     * @param listener The listener to add
+     * @return {@code true} if this bot did not already have this listener  
+     */
+    public boolean addListener(ListenerAdapter listener) {
+        boolean result = listeners.add(listener);
+        checkRep();
+        return result;
+    }
+    
+    /**
+     * Get an unmodifiable view of this bot's listeners.
+     * 
+     * @return This bot's listeners
+     */
+    public Set<ListenerAdapter> getListeners() {
+        return Collections.unmodifiableSet(listeners);
+    }
+    
+    /**
+     * Add an intent to this bot.
+     * 
+     * @param intent The intent to add
+     * @return {@code true} if this bot did not already have this intent
+     */
+    public boolean addIntent(GatewayIntent intent) {
+        boolean result = intents.add(intent);
+        checkRep();
+        return result;
+    }
+    
+    /**
+     * Get an unmodifiable view of this bot's intents.
+     * 
+     * @return this bot's intents.
+     */
+    public Set<GatewayIntent> getIntents() {
+        return Collections.unmodifiableSet(intents);
+    }
+}
---
+++ src/main/java/me/colingreybosh/cocoa/DataTable.java
@@ -0,0 +1,183 @@
+package me.colingreybosh.cocoa;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Mutable ADT representing a simple mapping of data stored in a file.
+ * 
+ * Files are formatted according to the following grammar, that requires a file
+ * to include newline terminated rows of two non-empty strings of non-whitespace, non-newline
+ * characters separated by a single space:
+ * 
+ * FILE ::= ROW*
+ * ROW ::= KEY " " VALUE NEWLINE
+ * KEY ::= VALUE
+ * VALUE ::= [^\s\n\r]+
+ * NEWLINE ::= "\n" | "\r" "\n"?
+ * 
+ * Completely empty files are valid tables as well.
+ * 
+ * @author Colin Greybosh
+ *
+ */
+public class DataTable implements Map<String, String>, Iterable<DataTable.Row>, AutoCloseable {
+    
+    /**
+     * An ADT representing a row within a 2-column table.
+     * 
+     * @author Colin Greybosh
+     * 
+     */
+    public static class Row {
+        private final String key;
+        private final String value;
+        
+        public Row(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+        
+        public String getKey() {
+            return key;
+        }
+        
+        public String getValue() {
+            return value;
+        }
+        
+        @Override
+        public boolean equals(Object that) {
+            return that instanceof Row && sameValue((Row) that);
+        }
+        
+        public boolean sameValue(Row that) {
+            return getKey().equals(that.getKey()) 
+                    && getValue().equals(that.getValue());
+        }
+        
+        @Override
+        public int hashCode() {
+            return Objects.hash(getKey(), getValue());
+        }
+        
+        @Override
+        public String toString() {
+            return "(" + getKey() + " -> " + getValue() + ")";
+        }
+    }
+    
+    /**
+     * Create a table, generating a new empty table at {@code pathToFile} if one
+     * does not already exist or parsing existing information.
+     * 
+     * Only a single instance of a table may exist for every unique file.
+     * 
+     * @param pathToFile The path to the file containing the table information.
+     */
+    public DataTable(String pathToFile) {
+        throw new RuntimeException("unimplemented");
+    }
+    
+    @Override
+    public void close() throws Exception {
+        throw new RuntimeException("unimplemented");
+    }
+    
+    /**
+     * Get the size of the table, in number of rows.
+     * 
+     * @return The size of the table, in number of rows.
+     */
+    public int size() {
+        throw new RuntimeException("unimplemented");
+    }
+    
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        throw new RuntimeException("unimplemented");
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        throw new RuntimeException("unimplemented");
+    }
+
+    @Override
+    public String get(Object key) {
+        throw new RuntimeException("unimplemented");
+    }
+
+    @Override
+    public String put(String key, String value) {
+        throw new RuntimeException("unimplemented");
+    }
+
+    @Override
+    public String remove(Object key) {
+        throw new RuntimeException("unimplemented");
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ? extends String> m) {
+        throw new RuntimeException("unimplemented");
+    }
+
+    @Override
+    public void clear() {
+        throw new RuntimeException("unimplemented");
+    }
+
+    @Override
+    public Set<String> keySet() {
+        throw new RuntimeException("unimplemented");
+    }
+
+    @Override
+    public Collection<String> values() {
+        throw new RuntimeException("unimplemented");
+    }
+
+    @Override
+    public Set<Entry<String, String>> entrySet() {
+        throw new RuntimeException("unimplemented");
+    }
+    
+    @Override
+    public Iterator<Row> iterator() {
+        throw new RuntimeException("unimplemented");
+    }
+    
+    @Override
+    public boolean equals(Object that) {
+        return that instanceof DataTable && sameValue((DataTable) that);
+    }
+    
+    /**
+     * Compares the current value of this table to {@code that}.
+     *  
+     * @param that Another table to compare equality to. 
+     * @return {@code true} if this table and {@code that} are observationally equal.
+     */
+    public boolean sameValue(DataTable that) {
+        throw new RuntimeException("unimplemented");
+    }
+    
+    @Override
+    public int hashCode() {
+        throw new RuntimeException("unimplemented");
+    }
+    
+    @Override
+    public String toString() {
+        throw new RuntimeException("unimplemented");
+    }
+}
