7510f5fe83e5ad7d80310d5c9a5994cdf2d1528f
Colin Greybosh <greybosh@mit.edu>
2020-05-29 12:48:26 -0400

    Removed methods from testing strategy

--- src/test/java/me/colingreybosh/cocoa/DataTableTest.java
+++ src/test/java/me/colingreybosh/cocoa/DataTableTest.java
@@ -25,45 +25,14 @@ public class DataTableTest {
     /*
      * Testing Strategy
      * 
+     * toFile()
+     *   
+     * 
+     * toMap()
+     *   
+     * 
      * size()
      *   returns 0, 1, >1
-     * 
-     * isEmpty()
-     *   returns true, false
-     * 
-     * containsKey(Object key)
-     *   returns true, false
-     * 
-     * containsValue(Object value)
-     *   returns true, false
-     * 
-     * get(Object key)
-     *   key is in table, key isn't in table
-     * 
-     * put(Object key, Object value)
-     *   key was already in table, key wasn't already in table
-     * 
-     * remove(Object key)
-     *   key was in table, key wasn't in table
-     * 
-     * putAll(Map<? extends String, ? extends String> m)
-     *   m is empty, m is nonempty
-     *   m contains new keys, m has same keys as table
-     * 
-     * clear()
-     *   table was empty, table wasn't empty
-     * 
-     * keySet()
-     *   table is empty, table isn't empty
-     * 
-     * values()
-     *   table is empty, table isn't empty
-     * 
-     * entrySet()
-     *   table is empty, table isn't empty
-     * 
-     * iterator()
-     *   table.size() == 0, == 1, > 1
      * 
      * sameValue()
      *   returns true, returns false
