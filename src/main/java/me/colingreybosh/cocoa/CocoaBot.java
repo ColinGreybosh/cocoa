5b597fd7c8fe154a1f7f4327df8213f38c191a9b
Colin Greybosh <greybosh@mit.edu>
2020-05-29 11:04:10 -0400

    Cleaned up TODO and prints

--- src/main/java/me/colingreybosh/cocoa/CocoaBot.java
+++ src/main/java/me/colingreybosh/cocoa/CocoaBot.java
@@ -17,19 +17,13 @@ public class CocoaBot extends Bot {
     
     public CocoaBot(String token) {
         super(token, Set.of(new CocoaBotListener()), Set.of(GatewayIntent.GUILD_MEMBERS));
-        // TODO listeners and intents
     }
     
     public static void main(String[] arguments) {
-        System.err.println("Creating bot...");
         final CocoaBot bot = new CocoaBot(Constants.TOKEN);
-        System.err.println("Bot created!");
-        System.err.println("Connecting bot...");
         try {
             bot.start();
-            System.err.println("Bot logged in!");
         } catch (LoginException | IllegalArgumentException | InterruptedException e) {
-            System.err.println("Bot failed to log in!");
             e.printStackTrace();
         }
     }
