be646e7374346b825d92a9ae50ae6fa486f4f070
Colin Greybosh <greybosh@mit.edu>
2020-06-04 14:51:55 -0400

    Added path to table for this specific project

--- src/main/java/me/colingreybosh/cocoa/Constants.java
+++ src/main/java/me/colingreybosh/cocoa/Constants.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
  * Constants required for this bot to operate properly.
  */
 public final class Constants {
-    public static final Pattern COCOA_REGEX = Pattern.compile("[cC]+[oO]+[cC]+[oO]+[aA]+"); 
-    public static final String PATH_TO_DATA = "" /* TODO fill in with path to file holding guild member data */; 
+    public static final Pattern COCOA_REGEX = Pattern.compile("[cC]+[oO]+[cC]+[oO]+[aA]+");
+    // Fill in with path to file holding guild member data
+    public static final String PATH_TO_DATA = "./src/main/java/me/colingreybosh/cocoa/tables/members.dt"; 
 }
