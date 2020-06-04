0efa3ca7792e8b1beba0e5dbe939df4bf241fa30
Colin Greybosh <greybosh@mit.edu>
2020-06-04 14:44:35 -0400

    Now assuming the token is assigned as a GitHub secret

--- src/main/java/me/colingreybosh/cocoa/Constants.java
+++ src/main/java/me/colingreybosh/cocoa/Constants.java
@@ -7,6 +7,5 @@ import java.util.regex.Pattern;
  */
 public final class Constants {
     public static final Pattern COCOA_REGEX = Pattern.compile("[cC]+[oO]+[cC]+[oO]+[aA]+"); 
-    public static final String TOKEN = "" /* TODO fill in with Discord bot token */;
     public static final String PATH_TO_DATA = "" /* TODO fill in with path to file holding guild member data */; 
 }
